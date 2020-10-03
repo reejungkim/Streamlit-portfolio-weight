579ecb09aebee416f9cfe40a9fa27aa2b48a2efb
reejung kim <reejung.kim@gmail.com>
2020-10-03 20:40:14 +0100

    Edit app

--- app.py
+++ app.py
@@ -12,70 +12,57 @@ from pandas_datareader import data as pdr
 #import yfinance as yf
 import streamlit as st
 
-date_start = '2020-01-01'
-today = dt.datetime.now() 
-  
-file = 'https://raw.githubusercontent.com/reejungkim/Streamlit/master/S%26P100%20tickers.csv'
-SP100_tickers = pd.read_csv(file,  error_bad_lines=False)
-#print(SP100_tickers['Symbol'])
-
-
 st.write("""
 # Simple Stock Price App
 Shown are the stock closing price and volume of S&P 100 stocks!
 """)
 
 
-# %%time
+# TICKERS
+file = 'https://raw.githubusercontent.com/reejungkim/Streamlit/master/S%26P100%20tickers.csv'
+SP100_tickers = pd.read_csv(file,  error_bad_lines=False)
 
-df = pd.DataFrame()
+tickers_selected = st.multiselect("Select ticker(s)", SP100_tickers.Symbol)
+tickers_selected =['AAPL']
+tickers_df = pd.DataFrame (tickers_selected,columns=['ticker'])
 
-for i in SP100_tickers['Symbol']:
-    symbol = SP100_tickers.loc[SP100_tickers['Symbol']==i]
-    try:
-        symbol_data = pdr.DataReader(i, 'yahoo', date_start, today).reset_index()
-        #display(symbol_data)  
-    except (KeyError, ValueError):  # the error could possibly occur when there's "." in stock name 
-        symbol_data = pdr.DataReader(i.replace('.','-'), 'yahoo', date_start, today).reset_index()
-        #symbol_data = pd.DataFrame()
-        pass
-    except:
-        print(i + " - Error.")
-        symbol_data = pd.DataFrame()
-        pass
-    single_table = pd.concat([symbol, symbol_data], axis=0, ignore_index=True) #axis=0 <- row. add frames by row and use fill down.
-    single_table['Symbol'].ffill(inplace=True)
-    df = df.append(single_table)
-    
-    
-df = df.loc[df['Date'].notnull()]
-df = df.reset_index(drop=True)   
+
+# DATES
+today = dt.datetime.now()
+start_date = st.sidebar.date_input('Start date', today - dt.timedelta(days=30))
+end_date = st.sidebar.date_input('End date', today - dt.timedelta(days=1)) 
+
+if start_date < end_date:
+    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+else:
+    st.error('Error: End date must fall after start date')
 
 
 
-#st.line_chart(df.Close)
-#st.line_chart(df.Volume)
 
-
-
-# Date inputs
-#start_date = st.date_input('Start date', today - dt.timedelta(days=1))
-#end_date = st.date_input('End date', today)
-
-#if start_date < end_date:
-#    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
-#else:
-#    st.error('Error: End date must fall after start date')
-
-d3 = st.date_input("range, no dates", [])
-st.write(d3)
-
-d3 = st.date_input("Range, one date", [dt.date(2020, 1, 1)])
-st.write(d3)
-
-d5 = st.date_input("date range without default", [dt.date(2020, 1, 1), dt.date(2020, 1, 7)])
-st.write(d5)
-
+if( tickers_selected != [] ):
+# LOAD DATA
+    df = pd.DataFrame()
+    for i in tickers_selected:
+        symbol = tickers_df.loc[tickers_df['ticker']==i]
+        try:
+            symbol_data = pdr.DataReader(i, 'yahoo', start_date, end_date).reset_index()
+            #display(symbol_data)  
+        except (KeyError, ValueError):  # the error could possibly occur when there's "." in stock name 
+            symbol_data = pdr.DataReader(i.replace('.','-'), 'yahoo', start_date, end_date).reset_index()
+            #symbol_data = pd.DataFrame()
+            pass
+        except:
+            print(i + " - Error.")
+            symbol_data = pd.DataFrame()
+            pass
+        single_table = pd.concat([symbol, symbol_data], axis=0, ignore_index=True) #axis=0 <- row. add frames by row and use fill down.
+        single_table['ticker'].ffill(inplace=True)
+        df = df.append(single_table)     
+    df = df.loc[df['Date'].notnull()]
+    df = df.reset_index(drop=True)   
+    st.line_chart(df.Close)
+    #st.line_chart(df.Volume)
 
 
 
