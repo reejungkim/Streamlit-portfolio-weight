5cef5dc95e91b0e22f2f5de4078a99915fbdb63f
reejung kim <reejung.kim@gmail.com>
2020-10-03 11:20:13 +0100

    Edit app

--- app.py
+++ app.py
@@ -5,32 +5,62 @@ Spyder Editor
 This is a temporary script file.
 """
 
-
-
-import yfinance as yf
+import datetime as dt
+import pandas as pd
+#to get financial data
+from pandas_datareader import data as pdr
+#import yfinance as yf
 import streamlit as st
+
+date_start = '2020-01-01'
+date_end = dt.datetime.now() 
+  
+
+SP100_tickers = pd.read_csv('/Users/reejungkim/Documents/Git/Heroku_deployment/Streamlit/testingstreamlit/S&P100 tickers.csv')
+print(SP100_tickers['Symbol'])
+
 
 st.write("""
 # Simple Stock Price App
 Shown are the stock closing price and volume of Google!
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2020-01-01', end='2020-09-01')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+# %%time
+
+df = pd.DataFrame()
+
+for i in SP100_tickers['Symbol']:
+    symbol = SP100_tickers.loc[SP100_tickers['Symbol']==i]
+    try:
+        symbol_data = pdr.DataReader(i, 'yahoo', date_start, date_end).reset_index()
+        #display(symbol_data)  
+    except (KeyError, ValueError):  # the error could possibly occur when there's "." in stock name 
+        symbol_data = pdr.DataReader(i.replace('.','-'), 'yahoo', date_start, date_end).reset_index()
+        #symbol_data = pd.DataFrame()
+        pass
+    except:
+        print(i + " - Error.")
+        symbol_data = pd.DataFrame()
+        pass
+    single_table = pd.concat([symbol, symbol_data], axis=0, ignore_index=True) #axis=0 <- row. add frames by row and use fill down.
+    single_table['Symbol'].ffill(inplace=True)
+    df = df.append(single_table)
+    
+    
+df = df.loc[df['Date'].notnull()]
+df = df.reset_index(drop=True)   
+
+
+
+st.line_chart(df.Close)
+st.line_chart(df.Volume)
+
+
 
 
 st.write("""
-    | Price | DVolume |
+    | Price | Volume |
     | ------|:-------|
 
 """    )
-
